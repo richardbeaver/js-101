85ebcc18bffca146d67f13417394045cf1a2ff40
Richard Beaver <beaver.r@northeastern.edu>
2023-05-02 13:40:57 -0400

    Add list of commands so far

--- lesson-2/debug.js
+++ lesson-2/debug.js
@@ -1,3 +1,15 @@
+/*
+
+Commands:
+
+node inspect <file-name> : Run node debugger
+exec <variable-name> : access value of given variable
+n : (next) run next line
+c : (cont) run until the end, an error, or a `debugger` statement is reached
+run/restart : run program from beginning (? - not sure on difference/details)
+
+*/
+
 let counter = 1;
 
 while (counter <= 5) {
