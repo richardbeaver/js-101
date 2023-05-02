4e3571ed1733e224794ca7e8c7796fb9468e8fa7
Richard Beaver <beaver.r@northeastern.edu>
2023-05-02 13:53:03 -0400

    Let user do more than one calculation

--- lesson-2/calculator.js
+++ lesson-2/calculator.js
@@ -12,52 +12,66 @@ function invalidNumber(number) {
 
 prompt('Welcome to Calculator!');
 
-// Get first number
+while (true) {
+  // Get first number
+  prompt("What's the first number?");
+  let number1 = readline.question();
 
-prompt("What's the first number?");
-let number1 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
 
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+  // Get second number
+  prompt("What's the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+
+  // Get operation to perform
+  prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3, or 4');
+    operation = readline.question();
+  }
+
+  // Perform operation
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  // Print result
+  prompt(`The result is: ${output}`);
+
+  // Ask if user wants to do another calculation
+  prompt('Would you like to do another calculation? (y/n)');
+  let answer = readline.question();
+
+  // Only accepting 'y' or 'n' as answers:
+  // while (!['y', 'n'].includes(answer)) {
+  //   prompt('Please enter "y" for yes or "n" for no.');
+  //   answer = readline.question();
+  // }
+
+  // if (answer !== 'y') break;
+
+  // Any answer that does not start with 'y' or 'Y' is a no:
+  if (answer[0].toLowerCase() !== 'y') break;
 }
-
-// Get second number
-
-prompt("What's the second number?");
-let number2 = readline.question();
-
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-// Get operation to perform
-
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3, or 4');
-  operation = readline.question();
-}
-
-// Perform operation
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-// Print result
-prompt(`The result is: ${output}`);
